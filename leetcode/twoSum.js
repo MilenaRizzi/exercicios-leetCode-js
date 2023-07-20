19f4963a4eb743809d44ee8040cae1c1ea1c8166
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2023-07-20 10:29:05 -0300

    Update and rename duasSomas.js to twoSum.js

---
+++ leetcode/twoSum.js
@@ -0,0 +1,22 @@
+//retornar os indices dos numeros que somados dão o valor de alvo
+function soma(nums, alvo) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    
+    if (map.hasOwnProperty(complement)) {
+      return [map[complement], i];
+    }
+    
+    map[nums[i]] = i;
+  }
+  
+  return [];
+}
+
+console.log(soma([2, 7, 11, 15], 9));
+console.log(soma([3, 2, 4], 6));
+console.log(soma([3, 3], 6));
+console.log(soma([2, 5, 5, 11], 10));
+console.log(soma([-1, -2, -3, -4, -5], -8));
